312f1f6b69029662980b88b32a9962b224bb384f
farhad20x <farhadshakiba@yahoo.com>
2020-04-27 09:41:09 +0430

    Initial commit

---
+++ blog/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from blog.api.serializers import BlogPostSerializer
+from blog.models import BlogPost
+from account.models import Account
+
+
+@api_view(['GET', ])
+def api_detail_blog_view(request, slug):
+    try:
+        blog_post = BlogPost.objects.get(slug=slug)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = BlogPostSerializer(blog_post)
+        return Response(serializer.data)
